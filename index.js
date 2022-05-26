f51db4ef655ca3e33e1a32c4daddce377ca2318a
rahul9232 <rahul9232singh@kgpian.iitkgp.ac.in>
2022-05-26 20:09:15 +0530

    heroku port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 app.set('view engine', 'ejs')
 
-//app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/public')))
 
 const PORT = process.env.PORT || 3000
 
